5e423263e15aa23d1dce05ce49d5269e281e899a
spatel1009 <spatel1009@gmail.com>
2018-08-29 09:39:04 -0400

    final

--- books/app/controllers/users_controller.rb
+++ books/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -7,5 +11,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end 
 
-  
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success]="Create Successful"
+    redirect_to books_path
+    else
+      flash[:error]=@user.errors
+    redirect_to users_new_path
+    end
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit(:user_name, :password)
+  end
 end
--- books/app/models/user.rb
+++ books/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :books
     validates_presence_of :user_name, length: {minimum: 5}
+    validates_uniqueness_of :user_name
 end
